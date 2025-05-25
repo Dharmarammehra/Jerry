e3d71a057877df1acd424e9fc665095e3dad92ce
Dharmarammehra <126263550+Dharmarammehra@users.noreply.github.com>
2025-05-25 14:09:32 +0530

    Update config.py
    Signed-off-by: Dharmarammehra <126263550+Dharmarammehra@users.noreply.github.com>

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/masoom70/Jerry",
+    "https://github.com/Dharmarammehra/Jerry",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
